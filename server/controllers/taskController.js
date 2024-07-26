0eeff31f5a136fc244dd0d56dd5ded746fe090e1
Ashmit Jain <ashmitjain25@gmail.com>
2024-07-26 23:08:46 +0530

    TaskRoutes post req

---
+++ server/controllers/taskController.js
@@ -0,0 +1,8 @@
+// export const registerUser = async (req, res) => {
+//   try {
+//   } catch (error) {
+//     return res
+//       .status(400)
+//       .json({ status: false, message: "Invalid user data" });
+//   }
+// };
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,4 +1,8 @@
 import express from "express";
 const router = express.Router();
 
+router.post("/create", protectedRoute, isAdminRoute, createTask);
+router.post("/duplicate/:id", protectedRoute, isAdminRoute, duplicateTask);
+router.post("/activity/:id", protectedRoute, postTaskActivity);
+
 export default router;
