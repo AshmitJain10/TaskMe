6d72653ddff463b4191211d5a0e5644a5cdbcbfb
Ashmit Jain <ashmitjain25@gmail.com>
2024-07-28 00:02:28 +0530

    Create Task - API

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,64 @@
-// export const registerUser = async (req, res) => {
+import Notice from "../models/notification.js";
+import Task from "../models/task.js";
+
+export const createTask = async (req, res) => {
+  try {
+    const { title, team, stage, date, priority, assets } = req.body;
+
+    const task = await Task.create({
+      title,
+      team,
+      stage: stage.toLowerCase(),
+      date,
+      priority: priority.toLowerCase(),
+      assets,
+    });
+    let text = "New task has been assigned to you";
+    if (task.team.length > 1) {
+      text = text + ` and ${task.team.length - 1} others.`;
+    }
+
+    text =
+      text +
+      ` The task priority is set as ${
+        task.priority
+      } priority, so check and act accordingly. The task date is ${task.date.toDateString()}.
+      HAPPY TASKING!!`;
+
+    await Notice.create({
+      team,
+      text,
+      task: task._id,
+    });
+
+    res
+      .status(200)
+      .json({ status: true, message: "Task Created Successfully." });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ status: false, message: error.message });
+  }
+};
+
+// export const createTask = async (req, res) => {
+//   try {
+//   } catch (error) {
+//     return res
+//       .status(400)
+//       .json({ status: false, message: "Invalid user data" });
+//   }
+// };
+
+// export const createTask = async (req, res) => {
+//   try {
+//   } catch (error) {
+//     return res
+//       .status(400)
+//       .json({ status: false, message: "Invalid user data" });
+//   }
+// };
+
+// export const createTask = async (req, res) => {
 //   try {
 //   } catch (error) {
 //     return res
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
+import { isAdminRoute, protectedRoute } from "../middlewares/authMiddleware.js";
+import { createTask } from "../controllers/taskController.js";
 const router = express.Router();
 
 router.post("/create", protectedRoute, isAdminRoute, createTask);
-router.post("/duplicate/:id", protectedRoute, isAdminRoute, duplicateTask);
-router.post("/activity/:id", protectedRoute, postTaskActivity);
+// router.post("/duplicate/:id", protectedRoute, isAdminRoute, duplicateTask);
+// router.post("/activity/:id", protectedRoute, postTaskActivity);
 
 export default router;
